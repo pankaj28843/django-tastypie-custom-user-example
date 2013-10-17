43bff3b469af84e462045084d0a32c39cf5dce0f
Pankaj Singh <psjinx@gmail.com>
2013-10-17 22:49:56 +0530

    Update models.py

--- django_tastypie_example/user_management/models.py
+++ django_tastypie_example/user_management/models.py
@@ -8,22 +8,22 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         '''
-        Creates and saves a User with the given username, email and password.
+        Creates and saves a User with the given email and password.
         '''
-        if not username:
-            raise ValueError('The given username must be set')
+        if not email:
+            raise ValueError('The given email must be set')
         email = UserManager.normalize_email(email)
-        user = self.model(username=username, email=email,
+        user = self.model(email=email,
                           is_staff=False, is_active=True, is_superuser=False,
                           **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password, **extra_fields):
-        u = self.create_user(username, email, password, **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        u = self.create_user(email, password, **extra_fields)
         u.is_staff = True
         u.is_active = True
         u.is_superuser = True
